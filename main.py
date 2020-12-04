2344384090bbc6c783ac25685634e56fbf04efb7
tabish hameed <tabishhameed34@gmail.com>
2020-12-04 19:42:15 +0000

    created one for class

--- main.py
+++ main.py
@@ -9,6 +9,17 @@ class FixedFloat:
     def from_sum(value1, value2):
         return  FixedFloat(value1 + value2)    
 
-
 new_number = FixedFloat.from_sum(19.58, 18.56)
 print(new_number)
+
+class dollar(FixedFloat):
+    def __init__(self,amount):
+     super().__init__(amount)
+     self.symbol = '$'
+    
+    def __repr__(self):
+        return f'<Dollar {self.symbol} {self.amount:.2f}>'
+
+    
+
+        
